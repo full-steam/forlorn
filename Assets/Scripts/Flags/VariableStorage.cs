bbc1e94f7864cd8f37dfc268b64c7e3967a2e788
Phobez <36761666+Phobez@users.noreply.github.com>
2021-05-07 16:01:03 +0700

    Added non-flag variables to VariableStorage.

--- Assets/Scripts/Flags/VariableStorage.cs
+++ Assets/Scripts/Flags/VariableStorage.cs
@@ -1,17 +1,25 @@
-﻿using Yarn;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Yarn;
 
 /// <summary>
 /// Custom YarnSpinner Variable Storage for integration with FlagManager.
 /// </summary>
 public class VariableStorage : Yarn.Unity.VariableStorageBehaviour
 {
+    private Dictionary<string, Value> variables = new Dictionary<string, Value>();
+
     /// <summary>
     /// Return a value, given a variable name.
     /// </summary>
     /// <param name="variableName">Variable name of the requested value.</param>
-    /// <returns></returns>
+    /// <returns>
+    /// If the variable name starts with "$", return a locally stored variable of type Value (Yarn's Value).
+    /// Else, returns the value of the flag named variableName.
+    /// </returns>
     public override Value GetValue(string variableName)
     {
+        if (variableName.StartsWith("$")) return variables[variableName];
         return new Value(GameManager.Instance.Blackboard.FlagManager.GetFlag(variableName));
     }
 
@@ -25,11 +33,27 @@ public class VariableStorage : Yarn.Unity.VariableStorageBehaviour
 
     /// <summary>
     /// Store a value into a variable.
+    /// If it's a locally-stored variable and the variable by the name variableName does not exist, it will be created.
     /// </summary>
     /// <param name="variableName">Variable name of the given value.</param>
     /// <param name="value">Value to be stored.</param>
     public override void SetValue(string variableName, Value value)
     {
+        if (variableName.StartsWith("$")) SetVariable(variableName, value);
         GameManager.Instance.Blackboard.FlagManager.SetFlag(variableName, value.AsBool);
+    }
+
+    /// <summary>
+    /// Sets the value of a locally stored variable.
+    /// If the variable by the name variableName does not exist, it will be created.
+    /// </summary>
+    /// <param name="variableName">Variable name of the given value. Must start with "$".</param>
+    /// <param name="value">Value to be stored.</param>
+    public void SetVariable(string variableName, Value value)
+    {
+        if (!variableName.StartsWith("$")) Debug.LogError("Locally-stored variables must start with a '$'!");
+
+        if (variables.ContainsKey(variableName)) variables[variableName] = new Value(value);
+        else variables.Add(variableName, new Value(value));
     }
 }
