7c6ddb6b7227676741953ac9962cbcc7d5797637
zalfornt <44518346+zalfornt@users.noreply.github.com>
2021-06-04 19:19:21 +0700

    Player Animations & changes to PlayerMovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
@@ -39,13 +39,37 @@ public class PlayerMovement : MonoBehaviour
             }
             else
             {
-                //bl_Joystick's Horizontal/Vertical returns float from -1 to 1. Manually converted to -1, 0 or 1
-                movement.x = (joystick.Horizontal > 0.1) ? 1 : ((joystick.Horizontal < -0.1) ? -1 : 0);
-                movement.y = (joystick.Vertical > 0.1) ? 1 : ((joystick.Vertical < -0.1) ? -1 : 0);
+                ////bl_Joystick's Horizontal/Vertical returns float from -1 to 1. Manually converted to -1, 0 or 1
+                //movement.x = (joystick.Horizontal > 0.1) ? 1 : ((joystick.Horizontal < -0.1) ? -1 : 0);
+                //movement.y = (joystick.Vertical > 0.1) ? 1 : ((joystick.Vertical < -0.1) ? -1 : 0);
+
+                movement.x = joystick.Horizontal;
+                movement.y = joystick.Vertical;
             }
-            //TODO: Add animation
-            //anim.SetFloat("Horizontal", movement.x);
-            //anim.SetFloat("Vertical", movement.y);
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+
+            #region Idle Direction
+            //sets up idle direction
+            // Down : 0
+            // Left : 1
+            // Up   : 2
+            // Right: 3
+            if (Mathf.Abs(movement.y) >= Mathf.Abs(movement.x))
+            {
+                if (movement.y < -0.1) anim.SetFloat("Direction", 0);
+                else if (movement.y > 0.1) anim.SetFloat("Direction", 2);
+            }
+            else
+            {
+                if (movement.x < -0.1) anim.SetFloat("Direction", 1);
+                else if (movement.x > 0.1) anim.SetFloat("Direction", 3);
+            }
+            #endregion
+
+            movement.x = (movement.x > 0.1) ? 1 : ((movement.x < -0.1) ? -1 : 0);
+            movement.y = (movement.y > 0.1) ? 1 : ((movement.y < -0.1) ? -1 : 0);
+
             //anim.SetFloat("Speed", movement.sqrMagnitude);
         }
     }
