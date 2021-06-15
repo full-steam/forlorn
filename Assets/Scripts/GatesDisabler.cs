84b40b07d7cb93d246aa647db30f49b4fcf9e520
Phobez <36761666+Phobez@users.noreply.github.com>
2021-06-15 18:17:23 +0700

    Created town.

---
+++ Assets/Scripts/GatesDisabler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GatesDisabler : MonoBehaviour
+{
+    public TilemapRenderer gateTower;
+
+    private new TilemapRenderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<TilemapRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            renderer.enabled = false;
+            gateTower.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            renderer.enabled = true;
+            gateTower.enabled = true;
+        }
+    }
+}
